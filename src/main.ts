a30e852e1b2307ab7d6f030cea3a5885ce1963ce
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-02-20 09:09:13 +0530

    angular and pnpm updated

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-
+import { enableProdMode } from "@angular/core";
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
+import { platformBrowser } from "@angular/platform-browser";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
-  .catch(err => console.log(err));
+platformBrowser()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
